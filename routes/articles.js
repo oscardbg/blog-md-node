f1e55fb0d8e246595bd8325238ac165167d29d3d
Dell <user@dell.com>
2020-08-04 09:03:45 -0500

    Article schema and save record to db

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const Article = require('../models/article');
+
 router.get('/new', (req, res) => {
-    res.render('articles/new');
+    res.render('articles/new', { article: new Article() });
 })
 
-router.post('/', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id);
     
+    if(article == null) res.redirect('/');
+    res.send(req.params.id);
+    //res.render('articles/show', {article})
+})
+
+router.post('/', async (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    
+    try {
+        article = await article.save();
+        res.redirect(`/articles/${article.id}`);
+    } catch (error) {
+        res.render('articles/new', { article });
+    }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const articleRouter = require('./routes/articles');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: false}));
+
+mongoose.connect('mongodb://localhost/blogdb', { useNewUrlParser: true,  useUnifiedTopology: true });
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
